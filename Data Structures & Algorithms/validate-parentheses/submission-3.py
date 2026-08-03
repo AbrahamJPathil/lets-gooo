a3fcda68102cb0dfeb3f2b5a505658b9c4886392
Abraham Jacob Pathil <abrahamjpathildev@gmail.com>
2026-08-03 07:55:17 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if(len(s)%2 != 0):
+            return False
+        coll = deque()
+        for c in s:
+            if(c == '('):
+                coll.append(')')
+            elif(c == '{'):
+                coll.append('}')
+            elif(c == '['):
+                coll.append(']')
+            
+            else:
+                if(len(coll) == 0):
+                    return False #started with end logic
+                lastEle = coll[-1]
+                if(lastEle != c):
+                    return False
+                else:
+                    coll.pop()
+        
+        return len(coll) == 0
+        
